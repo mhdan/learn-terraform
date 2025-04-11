1fbde97befb96db18e3bb2dd18df0c6ccfa7ff12
mzDAN <mh.ziaei.dan@gmail.com>
2025-04-11 17:11:35 +0330

    Add auto.tfvars file

---
+++ terraform.auto.tfvars
@@ -0,0 +1,4 @@
+zone_id = "5962c212dcd3ecee048bfc57ecbdeec7"
+name    = "chert-pori.mzdan.live"
+content = "10.163.44.55"
+comment = "chert record generated through VCS"
--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@
 
 terraform {
 
-/*
+  /*
   cloud {
     organization = "tutorial-HCP-mzdan"
 
@@ -11,7 +11,7 @@ terraform {
       name = "learn-terraform"
     }
   }
-*/
+  */
 
   # required_providers {
   #   aws = {
@@ -22,7 +22,7 @@ terraform {
 
   required_providers {
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
       version = "~> 5.3.0"
     }
   }
